337356532e40026b11e1383cf6a8a749d1c143ae
Ak-Shaw <ayushshawz@gmail.com>
2022-08-28 04:24:18 +0530

    feat(task): add classes for Note and Task

--- taskmanager/task/models.py
+++ taskmanager/task/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+"""NOTE:
+Since this project revolves around an in memory (NO DATABASE) app, we don't need to use django models
+"""
+
+
+class Note:
+    title = ""
+    body = ""
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+class Task:
+    id = 0
+    title = ""
+    description = ""
+    deadline = "28/08/2022"
+    notes = []
+    completed = False
+
+    def __init__(self, id, title, description, deadline, notes, completed):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.deadline = deadline
+        self.notes = notes
+        self.completed = completed
