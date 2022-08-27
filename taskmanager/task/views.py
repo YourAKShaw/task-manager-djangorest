00ec4c3753d49a3be60b87d6162be6accb1a5ccb
Ak-Shaw <ayushshawz@gmail.com>
2022-08-28 04:54:45 +0530

    feat(task): add GET route to get all tasks

--- taskmanager/task/views.py
+++ taskmanager/task/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+tasks = []
+
+
+@api_view(['GET'])
+def get_all_tasks(req):
+    return Response({
+        "statusCode": 200,
+        "message": "success",
+        "data": tasks
+    })
