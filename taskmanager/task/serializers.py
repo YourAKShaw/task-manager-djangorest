0a4095659863bb60b9739aebe5817385d36dad1f
Ak-Shaw <ayushshawz@gmail.com>
2022-09-01 10:02:22 +0530

    feat(task): add UpdateTaskSerializer and fixes to CreateTaskSerializer, also refactor NoteSerializer and TaskSerializer

--- taskmanager/task/serializers.py
+++ taskmanager/task/serializers.py
@@ -3,21 +3,31 @@ from task.models import Task, Note
 
 
 class NoteSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Note
-        fields = ('title', 'body')
+        fields = '__all__'
+
 
 class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description',
-                  'deadline', 'notes', 'completed')
+        fields = '__all__'
 
 
 class CreateTaskSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100, required=True)
-    description = serializers.CharField()
-    deadline = serializers.RegexField(regex=r'^\d{2}\/\d{2}\/\d{4}$')
+    description = serializers.CharField(required=False)
+    deadline = serializers.RegexField(
+        regex=r'^\d{2}\/\d{2}\/\d{4}$', required=False)
     notes = NoteSerializer(required=False, many=True)
+
+
+class UpdateTaskSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100, required=False)
+    description = serializers.CharField(required=False)
+    deadline = serializers.RegexField(
+        regex=r'^\d{2}\/\d{2}\/\d{4}$', required=False)
+    notes = NoteSerializer(required=False, many=True)
+    completed = serializers.BooleanField(required=False)
