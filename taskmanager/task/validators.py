5fb685794a388a503bb442408075f10894db4f5a
Ak-Shaw <ayushshawz@gmail.com>
2022-09-01 10:02:54 +0530

    feat(task): add validate_update_task_request_data()

--- taskmanager/task/validators.py
+++ taskmanager/task/validators.py
@@ -1,4 +1,4 @@
-from task.serializers import CreateTaskSerializer
+from task.serializers import CreateTaskSerializer, UpdateTaskSerializer
 
 
 def validate_create_task_request_data(request_data):
@@ -7,6 +7,20 @@ def validate_create_task_request_data(request_data):
         "errors": None
     }
     validation = CreateTaskSerializer(data=request_data)
+    if (validation.is_valid()):
+        pass
+    else:
+        validation_result["success"] = False
+        validation_result["errors"] = validation.errors
+    return validation_result
+
+
+def validate_update_task_request_data(request_data):
+    validation_result = {
+        "success": True,
+        "errors": None
+    }
+    validation = UpdateTaskSerializer(data=request_data)
     if (validation.is_valid()):
         pass
     else:
