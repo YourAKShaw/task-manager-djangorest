328aee045e634277c0ed3d14278741fb957d679d
Ak-Shaw <ayushshawz@gmail.com>
2022-09-01 06:52:01 +0530

    feat(task): add NoteSerializer and CreateTaskSerializer

--- taskmanager/task/serializers.py
+++ taskmanager/task/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from task.models import Task
+from task.models import Task, Note
 
+
+class NoteSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Note
+        fields = ('title', 'body')
 
 class TaskSerializer(serializers.ModelSerializer):
 
@@ -8,3 +14,10 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ('id', 'title', 'description',
                   'deadline', 'notes', 'completed')
+
+
+class CreateTaskSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100, required=True)
+    description = serializers.CharField()
+    deadline = serializers.RegexField(regex=r'^\d{2}\/\d{2}\/\d{4}$')
+    notes = NoteSerializer(required=False, many=True)
